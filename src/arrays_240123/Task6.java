cf2ecd5f900ba8e16ec1fe215c57bbd8a5f5722d
Ruslan Irzhanov <orsk_ruslan@live.com>
2023-01-25 23:30:29 +0300

    Add comment

--- src/arrays_240123/Task6.java
+++ src/arrays_240123/Task6.java
@@ -1,5 +1,4 @@
 package arrays_240123;
-
 public class Task6 {
 
     /*
@@ -12,9 +11,28 @@ public class Task6 {
             5.Индекс минимального элемента
      */
 
+    public static void sortArray(int[] array) {
+        int max_elem = 0;
+        int min_elem = 0;
 
+        for (int i : array) {
+            if (i > max_elem) max_elem = i;
+            if (i < min_elem) min_elem = i;
+        }
+        int difference = max_elem - min_elem;
+
+        System.out.println("Максимальный элемент массива: " + max_elem);
+        System.out.println("Минимальный элемент массива: " + min_elem);
+        System.out.println("Разница максимального и минимального значения: " + difference);
+
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] == max_elem) System.out.println("Индекс максимального значения: " + j);
+            if (array[j] == min_elem) System.out.println("Индекс минимального значения: " + j);
+        }
+    }
 
     public static void main(String[] args) {
-
+        int[] array = {-23, 7, 12, 89, 234, 67, -34, 88};
+        sortArray(array);
     }
 }
